07ac4df39fac346e1aa0c49552947d2fe4cd97ca
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-02-29 16:36:23 +0100

    feat(main.js): add function to save userName to sessionStorage

--- client/socket-client/main.js
+++ client/socket-client/main.js
@@ -4,20 +4,28 @@ const socket = io("http://localhost:3000");
 let sendMessage = document.querySelector("#sendMessage");
 let sendBtn = document.querySelector("#sendBtn");
 let chatList = document.querySelector("#chatList");
+let userNameInput = document.querySelector('#userName');
+let saveUserBtn = document.querySelector('#saveUser');
+
+saveUserBtn.addEventListener('click', () => {
+  let userName = userNameInput.value;
+  sessionStorage.setItem('user', userName);
+  userNameInput.value = '';
+});
 
 sendBtn.addEventListener("click", () => {
   console.log("send chat", sendMessage.value);
   socket.emit("chat", sendMessage.value);
-})
+});
 
 socket.on("chat", (arg) => {
   console.log("socket", arg);
 
   updateChat(arg);
-})
+});
 
 function updateChat(chat) {
   let li = document.createElement("li");
   li.innerText = chat;
   chatList.appendChild(li);
-}
+};
