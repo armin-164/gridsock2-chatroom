f7eaa15494104a84a64b57c58718003636e6ae16
armin-164 <arminsadr164@hotmail.com>
2024-03-01 16:03:40 +0100

    feat(displayChatRoom.js): Add displayChatRoom function
    Add the displayChatRoom function which will display all elements
    appropriate for a chat room.

---
+++ client/socket-client/scripts/displayChatRoom.js
@@ -0,0 +1,28 @@
+export default function displayChatRoom(room) {
+    document.body.innerHTML = "";
+
+    const chatPage = document.createElement("div");
+    chatPage.classList.add("chat-page");
+
+    chatPage.innerHTML = `
+    <nav class="nav-bar">
+        <h2>friendHub</h2>
+        <h3>${room}</h3>
+        <button class="leave-room-btn">Leave Room</button>
+    </nav>
+    <div class="chat-main-section">
+        <div class="send-message-container">
+            <div>
+                <label>Message:</label>
+                <input type="text" class="input-message">
+            </div>
+            <button class="send-message-button">SEND</button>
+        </div>
+
+        <div class="chat-box">
+        </div>
+    </div>
+    `
+
+    document.body.appendChild(chatPage)
+}
