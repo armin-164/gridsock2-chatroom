ac9d7da6e17ca220e923975b8458391fd71140bd
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-01 22:40:39 +0100

    :lipstick: style(base): adding base styling

--- client/socket-client/main.js
+++ client/socket-client/main.js
@@ -1,23 +1,24 @@
-import { io } from "socket.io-client";
-const socket = io("http://localhost:3000");
+import './css/style.scss';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:3000');
 
-let sendMessage = document.querySelector("#sendMessage");
-let sendBtn = document.querySelector("#sendBtn");
-let chatList = document.querySelector("#chatList");
+let sendMessage = document.querySelector('#sendMessage');
+let sendBtn = document.querySelector('#sendBtn');
+let chatList = document.querySelector('#chatList');
 
-sendBtn.addEventListener("click", () => {
-  console.log("send chat", sendMessage.value);
-  socket.emit("chat", sendMessage.value);
-})
+sendBtn.addEventListener('click', () => {
+  console.log('send chat', sendMessage.value);
+  socket.emit('chat', sendMessage.value);
+});
 
-socket.on("chat", (arg) => {
-  console.log("socket", arg);
+socket.on('chat', (arg) => {
+  console.log('socket', arg);
 
   updateChat(arg);
-})
+});
 
 function updateChat(chat) {
-  let li = document.createElement("li");
+  let li = document.createElement('li');
   li.innerText = chat;
   chatList.appendChild(li);
 }
