fa1f05e6edbb5a6644d4b8f6194e303e50caa903
armin-164 <arminsadr164@hotmail.com>
2024-03-01 16:09:19 +0100

    feat(updateRoomList.js): Configure socket connection and roomName click event
    Import io to be able to establish a socket connection.
    Initialize a socket connection to the provided URL.
    Add a click eventlistener to roomName which will first join the
    client to specific room and then execute the displayChatRoom
    function to display the chat.

--- client/socket-client/scripts/updateRoomList.js
+++ client/socket-client/scripts/updateRoomList.js
@@ -1,9 +1,17 @@
+import { io } from "socket.io-client";
+import displayChatRoom from "./displayChatRoom";
+const socket = io("http://localhost:3000");
+
 export default function updateRoomList(rooms) {
     const roomsContainer = document.querySelector(".rooms-container");
     roomsContainer.innerHTML = "";
     rooms.forEach(room => {
         const roomName = document.createElement("div");
         roomName.textContent = room;
+        roomName.addEventListener("click", () => {
+            socket.emit("join room", room);
+            displayChatRoom(room);
+        });
         roomsContainer.appendChild(roomName);
     });
 }
