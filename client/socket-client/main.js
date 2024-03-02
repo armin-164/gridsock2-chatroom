f4ecc4c7d60a47cfbdc1084ba8c2d02c3fcfd5a1
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-02 12:56:20 +0100

    :sparkles: feat(startPage): adding a js-rendered startpage to the website

--- client/socket-client/main.js
+++ client/socket-client/main.js
@@ -1,36 +1,36 @@
-import { io } from "socket.io-client";
-const socket = io("http://localhost:3000");
+import { io } from 'socket.io-client';
+import printStart from './scripts/printStart.mjs';
+const socket = io('http://localhost:3000');
 
-let sendMessage = document.querySelector("#sendMessage");
-let sendBtn = document.querySelector("#sendBtn");
-let chatList = document.querySelector("#chatList");
-let userNameInput = document.querySelector('#userName');
-let saveUserBtn = document.querySelector('#saveUser');
+// User state check
+if (sessionStorage.getItem('user')) {
+  // Add function to print rooms?
+  console.log('User stored in sessionStorage');
+} else {
+  printStart();
+}
 
-saveUserBtn.addEventListener('click', () => {
-  let userName = userNameInput.value;
-  sessionStorage.setItem('user', userName);
-  userNameInput.value = '';
-});
+/* let sendMessage = document.querySelector('#sendMessage');
+let sendBtn = document.querySelector('#sendBtn');
+let chatList = document.querySelector('#chatList');
 
-sendBtn.addEventListener("click", () => {
+sendBtn.addEventListener('click', () => {
   let user = sessionStorage.getItem('user');
-  console.log("send chat", sendMessage.value);
-  socket.emit("chat", {
+  console.log('send chat', sendMessage.value);
+  socket.emit('chat', {
     message: sendMessage.value,
-    user: user
+    user: user,
   });
 });
 
-socket.on("chat", (arg) => {
-  console.log("socket", arg);
+socket.on('chat', (arg) => {
+  console.log('socket', arg);
 
   updateChat(arg);
-}); 
-
+});
 
 function updateChat(chat) {
-  let li = document.createElement("li");
+  let li = document.createElement('li');
   li.innerText = chat.user + ': ' + chat.message;
   chatList.appendChild(li);
-};
+} */
