a89fdc765b7f1071caea4bf9d3c61232f4d44a51
jessworks <48979913+jessworks@users.noreply.github.com>
2024-03-05 03:23:57 +0100

    feat: chatList shows userName and inputMessage. Input field cleared at submission.

--- client/socket-client/scripts/displayChatRoom.js
+++ client/socket-client/scripts/displayChatRoom.js
@@ -1,4 +1,5 @@
 import displayMainPage from "./displayMainPage";
+import printStart from "./printStart";
 
 export default function displayChatRoom(room) {
     document.body.innerHTML = "";
@@ -16,16 +17,31 @@ export default function displayChatRoom(room) {
         <div class="send_message_container">
             <div>
                 <label>Message:</label>
-                <input type="text" class="input_message">
+                <input type="text" class="input_message" id="inputMessage">
             </div>
-            <button class="send_message_button">SEND</button>
+            <button class="send_message_button" id="sendMessageBtn">SEND</button>
         </div>
 
         <div class="chat_box">
+            <ul id="chatList">
         </div>
     </div>
     `
     document.body.appendChild(chatPage);
+
+    let inputMessage = document.querySelector("#inputMessage");
+    let sendMessageBtn = document.querySelector("#sendMessageBtn");
+
+    sendMessageBtn.addEventListener("click", function() {
+
+        let user = sessionStorage.getItem("user");
+
+        let li = document.createElement("li");
+        li.innerText = user + ": " + inputMessage.value;
+        chatList.appendChild(li);
+
+        inputMessage.value = "";
+    });
 
     const leaveRoomBtn = document.querySelector(".leave_room_btn");
     leaveRoomBtn.addEventListener("click", displayMainPage);
